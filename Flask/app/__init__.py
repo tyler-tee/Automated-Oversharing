7582a9c4581a828bbd0a2dca3670412488d5270d
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2024-11-06 10:26:46 -0500

    linting

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from sqlmodel import SQLModel, create_engine
 from .blog import blog_pages
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key_here'
