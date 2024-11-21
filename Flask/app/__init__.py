bef50ac69adf89d8284226580a6772dec2c0bb8c
tyler-tee <64701075+tyler-tee@users.noreply.github.com>
2024-11-21 06:44:34 -0500

    update to use env vars

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from sqlmodel import SQLModel, create_engine
 from .blog import blog_pages
@@ -5,7 +6,7 @@ from .blog import blog_pages
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key_here'
+    app.config['SECRET_KEY'] = os.getenv("flask_secret_key")
     app.engine = create_engine("sqlite:///database.db")
     SQLModel.metadata.create_all(app.engine)
 
